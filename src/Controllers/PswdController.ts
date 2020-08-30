35a50833287de32a3fe6b0982ee59a63bdcdfd56
iuri-pdista <correaiuri86@gmail.com>
2020-08-30 14:23:16 -0300

    Add password generation methods

--- src/Controllers/PswdController.ts
+++ src/Controllers/PswdController.ts
@@ -15,7 +15,7 @@ export default class PswdController {
         if (newPswd.Length == 0){
             return res.status(500).send("We're facing some troubles, please do have patience, and try again");
         }
-        return res.send(req.body);
+        return res.status(201).send(newPswd.passwordBody);
     }
 
     static ProcessSpecifications(length: number, specifications: PswdSpecifications):PswdModel {
@@ -24,22 +24,56 @@ export default class PswdController {
                 number,
                 symbols
             } = specifications;
-            console.log(length, number, symbols);
-            const newPswd: PswdModel = new PswdModel("", symbols, number, length);
+            const newPswd: PswdModel = new PswdModel(
+                    PswdController.GeneratePassword(length, number, symbols), 
+                    symbols, 
+                    number, 
+                    length);
             return newPswd;
         } 
         catch (error) {
             console.log(error);
-            return (new PswdModel("", false, false, 0));
+            return (new PswdModel([], false, false, 0));
         }
     }
 
-    // static GeneratePassword (req: Request, res: Response) {
-    //     try {
-            
-    //     } 
-    //     catch (error) {
-    //         console.log(error);
-    //     }
-    // }
+    static GeneratePassword ( length: number, number: boolean, symbols: boolean ): string[]{
+        try {
+            const password: string[] = [];
+            let i = 0;
+            while ( i < length ){
+                const rndNumber = Math.floor(Math.random()* 3);
+                password[i] = PswdController.NextCharDecision(number, symbols, rndNumber)
+                console.log(password[i], length, i, rndNumber);
+                i += 1;
+            }
+            return password;
+        } 
+        catch (error) {
+            console.log(error);
+            return [];
+        }
+    }
+
+    static NextCharDecision (number: boolean, symbols: boolean, rndNumber: number): string {
+        try {
+            if ( rndNumber == 0 ){
+                let generatedChar = PswdController.GenerateChar();
+                return generatedChar;
+            }
+            else if (rndNumber == 1 && number ){
+                return Math.floor(Math.random() * 10).toLocaleString();
+            }
+            else if ( rndNumber == 2 && symbols ){
+                return "&"            
+            }
+            else{
+                return "A"
+            }
+        } 
+        catch (error) {
+            console.log(error)
+            return "A"           
+        }
+    } 
 }
