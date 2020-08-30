8579dc6da03c62976d8fd5781f70f0d22a9b0188
iuri-pdista <correaiuri86@gmail.com>
2020-08-29 21:17:20 -0300

    Fixed CORS bug

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express = require("express");
-// import routes from './Routes/routes';
+// import cors = require('cors');
+import routes from './Routes/routes';
 const app = express();
 
+// app.use(cors);
 app.use(express.json());
-// app.use(routes);
+app.use(routes);
 
 app.listen(5500);
