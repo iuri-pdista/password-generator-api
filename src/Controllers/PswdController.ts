49069b965d69ac27fe6b970fe842bdd4fd1e1c0a
iuri-pdista <correaiuri86@gmail.com>
2020-08-29 21:16:58 -0300

    Created ProcessSpecifications method

--- src/Controllers/PswdController.ts
+++ src/Controllers/PswdController.ts
@@ -11,7 +11,18 @@ export default class PswdController {
         if ( !length || specifications.length == 0 ) {
             return res.status(404).send("BAD REQUEST");
         }
+        const newPswd: PswdModel = this.ProcessSpecifications(length, specifications[0]);
         return res.send(req.body);
+    }
+
+    static ProcessSpecifications(length: number, specifications: PswdSpecifications):PswdModel {
+        const {
+            numbers,
+            symbols
+        } = specifications;
+        console.log(length, numbers, symbols);
+        const newPswd: PswdModel = new PswdModel("", symbols, numbers, length);
+        return newPswd;
     }
 
     // static GeneratePassword (req: Request, res: Response) {
