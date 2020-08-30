8f56e3017e8960c6bfbafa72dd63f25b4699b106
iuri-pdista <correaiuri86@gmail.com>
2020-08-30 14:24:48 -0300

    Moved the routing logic to the server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express = require("express");
 // import cors = require('cors');
-import routes from './Routes/routes';
+import PswdController from './Controllers/PswdController';
 const app = express();
 
 // app.use(cors);
 app.use(express.json());
+const routes = express.Router();
+routes.post('/generate', PswdController.RecieveSpecifications);
+
 app.use(routes);
 
 app.listen(5500);
