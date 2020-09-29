d377ba90aecc7de66dee30fb67243d208442ea3e
iuri-pdista <correaiuri86@gmail.com>
2020-09-29 14:21:07 -0300

    Add UpperCase attribute to PswdModel and PswdSpecifica

--- src/Models/PswdModel.ts
+++ src/Models/PswdModel.ts
@@ -1,19 +1,22 @@
 export default class PswdModel {
-    constructor ( password: string[], symbols: boolean, numbers: boolean,length: number ){
+    constructor ( password: string[], symbols: boolean, numbers: boolean,length: number, upperCase: boolean ){
         this.passwordBody = password;
         this.Length = length;
         this.Symbols = symbols;
         this.Numbers = numbers;
+        this.UpperCase = upperCase;
     }
     passwordBody: string[];
     Length: number;
     Symbols: boolean;
     Numbers: boolean;
+    UpperCase: boolean;
 
     calculateStrength ():number {
         let Strength = this.Length;
         if ( this.Symbols ) { Strength +=1 }
         if ( this.Numbers ) { Strength += 1 }
+        if ( this.UpperCase ) { Strength += 1 }
         return Strength;
     }
 }
